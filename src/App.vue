3828b24d72ec10ef709bbcaa282fb3cfa018b6e0
semyonfox <semyon.fox@gmail.com>
2026-01-16 19:42:15 +0000

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
-<script setup></script>
+<script setup>
+
+  let i = "im an i"
+  console.log(i)
+</script>
 
 <template>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
-  </p>
+  </p> <p> </p>
 </template>
 
 <style scoped></style>
